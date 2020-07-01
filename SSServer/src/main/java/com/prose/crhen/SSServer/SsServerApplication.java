a3cc2f873f4fcf9c9012bdedf263636ee6eb42de
henricri <henricri@students.zhaw.ch>
2020-07-01 09:37:38 +0200

    First Commit

---
+++ SSServer/src/main/java/com/prose/crhen/SSServer/SsServerApplication.java
@@ -0,0 +1,13 @@
+package com.prose.crhen.SSServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsServerApplication.class, args);
+	}
+
+}
---
+++ SSServer/src/test/java/com/prose/crhen/SSServer/SsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prose.crhen.SSServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
