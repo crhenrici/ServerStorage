e8d5d2624bb1cc7fb6c83c0cf8939b1124d8df82
Cristian Henrici <henricri@students.zhaw.ch>
2020-11-09 13:30:00 +0100

    seperated scripts into two files

--- SSServer/src/main/scripts/Powershell/serverquery.ps1
+++ SSServer/src/main/scripts/Powershell/serverquery.ps1
@@ -1,21 +1,11 @@
-﻿$output = Get-WmiObject win32_volume | Select-Object SystemName, Name, DriveLetter,
-    @{ Name = "CapacityGB"; Expression = { [math]::round($_.Capacity / 1GB, 2) } },
-    @{ Name = "FreeSpaceGB"; Expression = { [math]::round($_.FreeSpace / 1GB, 2) } },
-    @{ Name = "FreeSpacePercent"; Expression = { [math]::round(($_.FreeSpace / ($_.Capacity * 1.00)) * 100.00, 2) } },
-    @{ Name = "Date"; Expression = { $( Get-Date -f s ) } } |
-        Sort-Object Name |
-        Convertto-JSON
-
-Invoke-RestMethod -Method Post -Uri "http://localhost:9010/service/save/volume" -Body ($output) -ContentType 'application/json'
-
 $ramOutput = Get-WmiObject win32_physicalmemory |
-        Select-Object @{n="Capacity";e={$_.Capacity/1GB}} |
-        ConvertTo-Json
+        Select-Object @{n="Capacity";e={$_.Capacity/1GB}}
+$cpuUsage = Get-Counter '\Processor(_total)\% Processor Time'|
+        Select-Object -expand CounterSamples
+$output = [ordered]@{
+    cpuusage = $cpuUsage
+    ram = $ramOutput
+}
+$jsonOut  = $output | ConvertTo-Json
 
-$cpuUsage = Get-Counter '\Processor(*)\% Processor Time'|
-        Select-Object -expand CounterSamples |
-        ConvertTo-Json
-
-Invoke-RestMethod -Method Post -Uri "http://localhost:9010/service/save/server" -Body ($ramOutput) -ContentType 'application/json'
-Invoke-RestMethod -Method Post -Uri "http://localhost:9010/service/save/server" -Body ($cpuUsage) -ContentType 'application/json
-'
+Invoke-RestMethod -Method Post -Uri "http://localhost:9010/service/save/server" -Body ($jsonOut) -ContentType 'application/json'
