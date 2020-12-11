07228c1777a9745f2bbe13bca4f5c89ff0e14a1e
Cristian Henrici <henricri@students.zhaw.ch>
2020-12-11 16:53:00 +0100

    adjusting overview site

--- SSClient/src/app/app.module.ts
+++ SSClient/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerListComponent } from './server-list/server-list.component';
-import { MatTableModule, MatDialogModule, MatSortModule, MatFormFieldModule, MatButtonModule, MatDividerModule } from '@angular/material';
+import { MatTableModule, MatDialogModule, MatSortModule, MatFormFieldModule, MatButtonModule, MatDividerModule, MatToolbarModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { DetailsViewComponent } from './details-view/details-view.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -30,6 +30,7 @@ import { OverallViewComponent } from './overall-view/overall-view.component';
     MatButtonModule,
     MatDividerModule,
     MatFormFieldModule,
+    MatToolbarModule,
     NoopAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
