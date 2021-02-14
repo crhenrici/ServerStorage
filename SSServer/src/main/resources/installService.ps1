1fc2fb84405556a975d03c0c029542680219dc36
Cristian Henrici <henricri@students.zhaw.ch>
2021-02-14 16:27:21 +0100

    deleted myapp.xml, adjusted installService and serverquery

--- SSServer/src/main/resources/installService.ps1
+++ SSServer/src/main/resources/installService.ps1
@@ -1,5 +1,8 @@
+param (
+    [string]$profile = "dev"
+)
 nssm install ServerStorage "java"
 nssm set ServerStorage AppDirectory C:\test
-nssm set ServerStorage AppParameters "-jar lib/SSServer-0.0.1-SNAPSHOT.jar --spring.config.location=config/ --spring.profiles.active=dev,testdata,mockdata,log"
+nssm set ServerStorage AppParameters "-jar lib/SSServer-0.0.1-SNAPSHOT.jar --spring.config.location=config/ --spring.profiles.active=$profile"
 nssm set ServerStorage DisplayName ServerStorage
-nssm set ServerStorage Description Service of ServerStorage
+nssm set ServerStorage Description Service of ServerStorage with
--- SSServer/src/main/scripts/Powershell/serverquery.ps1
+++ SSServer/src/main/scripts/Powershell/serverquery.ps1
@@ -3,12 +3,12 @@ $ramOutput = Get-WmiObject win32_physicalmemory |
 $ramUsage = (Get-WmiObject Win32_ComputerSystem).totalphysicalmemory / (1024 * 1024 * 1024)
 $cpuUsage = Get-Counter '\Processor(_total)\% Processor Time'|
         Select-Object -expand CounterSamples
-$sysyemName = Get-WmiObject win32_volume | Select-Object SystemName -ExpandProperty sysyemName -First 1
+$systemName = Get-WmiObject win32_volume | Select-Object SystemName -First 1
 $output = [ordered]@{
     CpuUsage = $cpuUsage
     Ram = $ramOutput
     RamUsage = $ramUsage
-    SystemName = $sysyemName
+    SystemName = $systemName
 }
 $jsonOut  = $output | ConvertTo-Json
 
