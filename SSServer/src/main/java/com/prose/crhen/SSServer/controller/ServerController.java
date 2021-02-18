d045851f809e82a731d9e4ef57809d6852f73205
Cristian Henrici <henricri@students.zhaw.ch>
2021-02-18 15:49:55 +0100

    fixed api endpoints to make it finally working

--- SSServer/src/main/java/com/prose/crhen/SSServer/controller/ServerController.java
+++ SSServer/src/main/java/com/prose/crhen/SSServer/controller/ServerController.java
@@ -6,15 +6,17 @@ import com.prose.crhen.SSServer.dto.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping(path = "service")
+@Controller
+//@CrossOrigin(origins = "http://localhost:4200")
+//@RequestMapping(path = "service")
 public class ServerController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ServerController.class);
@@ -22,33 +24,33 @@ public class ServerController {
 	@Autowired
 	private ServerService service;
 
-//	@RequestMapping(value = {"/error"})
-//	public String index() {
-//		return "forward:/index.html";
-//	}
+	@RequestMapping({"","/error","/servers"})
+	public String index() {
+		return "forward:/index.html";
+	}
 
-	@GetMapping("/overview")
+	@GetMapping("/service/overview")
 	@ResponseBody
 	public ServerOverviewDTO getServerOverview() {
 		ServerOverviewDTO serverOverviewDTO = service.getServerOverviewDTO();
 		return  serverOverviewDTO;
 	}
 
-	@GetMapping("/servers")
+	@GetMapping("/service/servers")
 	@ResponseBody
 	public List<ServerQueryDTO> getAllServers() {
 		List<ServerQueryDTO> serverList = service.getServers();
 		return serverList;
 	}
 
-	@GetMapping("/volumes")
+	@GetMapping("/service/volumes")
 	@ResponseBody
 	public List<VolumeQueryDTO> getAllVolumesFromServer(@RequestParam String serverName) {
 		List<VolumeQueryDTO> volumeList = service.getVolumesFromServer(serverName);
 		return volumeList;
 	}
 	
-	@PostMapping("/save/volume")
+	@PostMapping("/service/save/volume")
 	public ResponseEntity<String> saveVolumes(@RequestBody List<VolumesUpdateDTO> volumes) {
 		Preconditions.checkNotNull(volumes, "value can't be null");
 		logger.info(volumes.toString());
@@ -58,7 +60,7 @@ public class ServerController {
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
-	@PostMapping("/update/volume")
+	@PostMapping("/service/update/volume")
 	public ResponseEntity<String> updateVolume(@RequestBody VolumeQueryDTO volume) {
 		Preconditions.checkNotNull(volume, "value can't be null");
 		logger.info("Request received");
@@ -66,7 +68,7 @@ public class ServerController {
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
-	@PostMapping("/save/server")
+	@PostMapping("/service/save/server")
 	public ResponseEntity<String> saveServers(@RequestBody ServerUpdateDTO server) {
 		Preconditions.checkNotNull(server, "value can't be null");
 		logger.info("Data received");
@@ -74,5 +76,4 @@ public class ServerController {
 		service.saveServerDTO(server);
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
-
 }
--- SSServer/src/main/resources/installation/start.sh
+++ SSServer/src/main/resources/installation/start.sh
@@ -5,9 +5,9 @@ export DB_URI=$3
 export DB_NAME=$4
 java -jar lib/SSServer-0.0.1-SNAPSHOT.jar --spring.config.location=config/ --spring.resources.static-locations=web/static --spring.profiles.active=$5
 
-oder
-
-java -Dloader.path="web/" -jar lib/SSServer-0.0.1-SNAPSHOT.jar --spring.config.location=config/  --spring.profiles.active=$5
-
-
-ACHTUNG : untested stuff
+#oder
+#
+#java -Dloader.path="web/" -jar lib/SSServer-0.0.1-SNAPSHOT.jar --spring.config.location=config/  --spring.profiles.active=$5
+#
+#
+#ACHTUNG : untested stuff
