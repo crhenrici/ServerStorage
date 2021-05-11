82028295022651c331a1960d2c874f8a4db58773
Cristian Henrici <henricri@students.zhaw.ch>
2021-05-11 11:13:47 +0200

    fixed some changes

--- SSServer/src/main/java/com/prose/crhen/SSServer/business/api/PDFGenerator.java
+++ SSServer/src/main/java/com/prose/crhen/SSServer/business/api/PDFGenerator.java
@@ -1,10 +1,8 @@
 package com.prose.crhen.SSServer.business.api;
 
 import com.lowagie.text.DocumentException;
-<<<<<<< Updated upstream
 import com.prose.crhen.SSServer.dto.PdfDTO;
-=======
->>>>>>> Stashed changes
+
 
 import java.io.IOException;
 
